d044c341227886d038e4bd480d5be3becc12d00b
Nokking <japaneseplay1994@gmail.com>
2022-01-06 04:13:22 +0000

    Toppage designed Complete

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,15 +3,18 @@ class SettingsController < ApplicationController
     @user = current_user
   end
   
-   def update
-     @user = current_user
-     @user.update_attributes(params_user)
-     redirect_to user_url(@user)
-   end
+    def update
+      @user = current_user
+      if @user.update_attributes(params_user)
+        redirect_to user_url(@user), notice: "プロフィールを更新しました"
+      else
+        render :edit
+      end
+    end
  
-   private
+    private
  
-   def params_user
-     params.require(:user).permit(:name, :screen_name, :bio)
-   end
+    def params_user
+      params.require(:user).permit(:name, :screen_name, :bio)
+    end
 end
